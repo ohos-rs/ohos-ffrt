3664276017bab48dbca9baeb8224fb3728ede3f6
richerfu <southorange0929@foxmail.com>
2025-10-29 19:22:31 +0800

    fix: align call_local with call_async

--- crates/ext/src/tsfn_local/mod.rs
+++ crates/ext/src/tsfn_local/mod.rs
@@ -20,10 +20,8 @@ pub trait ThreadsafeFunctionExt {
     const WEAK: bool;
     const MAX_QUEUE_SIZE: usize;
 
-    fn call_local(
-        &self,
-        value: Result<Self::T, Self::ErrorStatus>,
-    ) -> impl std::future::Future<Output = Result<Self::Return>>;
+    fn call_local(&self, value: Self::T)
+    -> impl std::future::Future<Output = Result<Self::Return>>;
 }
 
 impl<
@@ -54,10 +52,9 @@ impl<
     const WEAK: bool = Weak;
     const MAX_QUEUE_SIZE: usize = MaxQueueSize;
 
-    async fn call_local(&self, value: Result<Self::T, Self::ErrorStatus>) -> Result<Self::Return> {
+    async fn call_local(&self, value: Self::T) -> Result<Self::Return> {
         let (sender, receiver) = ohos_ffrt::channel::<Result<Return>>();
-
-        self.handle().with_read_aborted(|aborted| {
+        self.handle.with_read_aborted(|aborted| {
             if aborted {
                 return Err(Error::from_status(Status::Closing));
             }
@@ -65,16 +62,20 @@ impl<
             check_status!(
                 unsafe {
                     sys::napi_call_threadsafe_function(
-                        self.handle().get_raw(),
-                        Box::into_raw(Box::new(value.map(|data| {
-                            ThreadsafeFunctionCallJsBackData {
-                                data,
-                                call_variant: ThreadsafeFunctionCallVariant::WithCallback,
-                                callback: Box::new(move |d: Result<Return>, _| {
-                                    sender.send(d).or(Ok(()))
-                                }),
-                            }
-                        })))
+                        self.handle.get_raw(),
+                        Box::into_raw(Box::new(ThreadsafeFunctionCallJsBackData {
+                            data: value,
+                            call_variant: ThreadsafeFunctionCallVariant::WithCallback,
+                            callback: Box::new(move |d, _| {
+                                d.and_then(|d| {
+                                    sender
+                                        .send(d)
+                                        // The only reason for send to return Err is if the receiver isn't listening
+                                        // Not hiding the error would result in a napi_fatal_error call, it's safe to ignore it instead.
+                                        .or(Ok(()))
+                                })
+                            }),
+                        }))
                         .cast(),
                         ThreadsafeFunctionCallMode::NonBlocking.into(),
                     )
