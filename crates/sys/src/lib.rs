fbe91ccaae287f9e2c6413e243888e78e123ec91
richerfu <southorange0929@foxmail.com>
2025-12-09 17:21:12 +0800

    build: cleanup the sys build script

--- crates/sys/src/lib.rs
+++ crates/sys/src/lib.rs
@@ -3,6 +3,9 @@
 #![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
 #![allow(clippy::missing_safety_doc)]
 
+#[link(name = "ffrt.z")]
+unsafe extern "C" {}
+
 pub type time_t = ::std::os::raw::c_long;
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
